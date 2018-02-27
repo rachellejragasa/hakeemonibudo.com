3c8a23469a0356c21a57b5eef3384de9388cf819
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-02-27 14:31:06 +0000

    Adding FontAwesome JS/SVG version

--- footer.php
+++ footer.php
@@ -6,9 +6,9 @@
         </div>  
         <div class="col s12 m12 l6 xl6">
           <ul class="page-footer__social-media">
-            <li><a href="https://twitter.com/hakeemonibudo"><i class="fa fa-fw fa-twitter"></i></a></li>
-            <li><a href="https://www.facebook.com/hakeem.onibudo"><i class="fa fa-fw fa-facebook"></i></a></li>
-            <li><a href="https://www.instagram.com/hakeemonibudo/"><i class="fa fa-fw fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/hakeemonibudo"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://www.facebook.com/hakeem.onibudo"><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href="https://www.instagram.com/hakeemonibudo/"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>  
         <div class="col s12 m12 l3 xl3">
@@ -22,6 +22,7 @@
   <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
   <script src="<?php echo get_template_directory_uri() . '/assets/js/init.js'; ?>"></script>
+  <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
   </body>
 </html>
