15350a91d69391d03a4173d9ebfd50954f26da4e
Rachelle Ragasa <rachelle.ragasa@whitbread.com>
2018-03-25 19:48:02 +0100

    Making credits section cms

--- index.php
+++ index.php
@@ -5,34 +5,10 @@
     <div class="row">
       <div class="container">
         <div class="col s12 m12 l6 xl6">
-          <h5 class="biography__heading">Hakeem is an experienced and professional event host who has been regularly presenting and 
-              entertaining on the red carpet at high profile film premiers in London since 2010.</h5>
+          <h5 class="biography__heading"><?php the_field('bio_heading');?></h5>
         </div>
         <div class="col s12 m12 l5 xl5">
-          <p>His movie
-              premiers include: Suicide Squad, Captain America - Civil War, Star Wars: The Force Awakens,
-              The Revenant, Minions, Avengers Age of Ultron, Hunger Games, Captain America - The Winter Solider,
-              Frozen plus many more. He regular works with clients such as Paramount Pictures, Sony Pictures,
-              Disney, Warner Brothers and Sassy Films.</p>
-
-          <p>Hakeem is also an accomplished peformer, choreographer/director, mentor and British Council
-              ambassador who began teaching dance in 1995, when he formed the company Impact Dance. Hakeem's
-              dance portfolio includes organisations such as BBC, Nickelodeon, CH4, Endemol, ITV, The Duke of
-              Edinburgh Awards, The Royal Ballet School, The Royal Opera House, Sadler's Wells, and the British
-              Council.</p>
-
-          <p>Hakeem is the artistic director and founder of Impact Dance and creator and host of LIVE VIBE, 
-              a performance platform created in 2008 with previous residency at Sadler’s Wells, Peacock Theatre 
-              and Rich Mix. The concept was taken to Hong Kong in December 2010/2013 and created and produced in 
-              partnership with the British Council in Zimbabwe in February 2012.</p>
-
-          <p>Hakeem has recently recieved the CHANGEMAKERS award by Arts Council England, which will allow 
-              him to lead on  a number of projects at the Place (Contemporary Dance Trust) over the next 18 months 
-              aiming at increasing the the number of Black, Asian and minority ethnic people who engage with the 
-              organisation.</p>
-
-          <p>An expert Master of Ceremonies (MC), Hakeem is spontaneous, versatile, quick thinking. Hakeem 
-              is available for broadcast media, corporate event hosting and artistic direction.</p>
+          <?php the_field('bio_intro_text');?>
         </div>
       </div>
     </div>
@@ -43,101 +19,37 @@
     <div class="row">
       <div class="container">
         <div class="col s12 m12 l3 xl3">
-            <h4 class="credits__heading">Credits</h4>
+            <h4 class="credits__heading"><?php the_field('credits_heading');?></h4>
         </div>
         <div class="col s12 m12 l9 xl9">
           <div class="credits__section">
-              <h5 class="credits__sub-heading">Red Carpet Movie Premieres
-                  <br>    
-                  Warm-up Host and Host/Master of Ceremonies
-              </h5>
-
-              <p>Suicide Squad, Captain America-Civil War, Absolutely Fabulous, Star Trek, Alice Through The Looking Glass, 
-                The Legend of Tarzan, The Revenant, Kung Fu Panda 3, Zoolander 2, The Jungle Book, Star Wars The Force Awakens, 
-                Avengers Age of Ultron, Enoturage, 50 Shades of Grey, Magic Mike XXL, Ant Man, Spy, Mortedecai, Focus, Sponge Bob 
-                The Movie, The Hunger Games Catching Fire, Plastic, The Amazing Spiderman-2, Captain America-The Winter Soldier, 
-                Noah, Wolf of Wall Street, Man of Steel, Arrested Development, Iron Man 3, Men in Black 3, Monuments Men, 
-                Fast and Furious 6, Star Trek Into Darkness, Hunger Games: Catching Fire, Frozen, The Lone Ranger, Saving Mr Banks, 
-                Thor Dark World, One Direction: This is us, The Hobbit: An Unexpected Journey, Rock of Ages, The Amazing Spider-Man, 
-                The Dark Knight Rises, Street Dance 3D, Street Dance 2</p>
+              <h5 class="credits__sub-heading"><?php the_field('credits_section_1_heading');?></h5>
+              <?php the_field('credits_section_1_text');?>
             </div>
 
             <div class="credits__section">
-              <h5 class="credits__sub-heading">Additional MC/Hosting</h5>
-
-              <p>MOVEIT-Olympia and EXCEL 2010-Present
-                <br>
-                Bobby Dazzlers Her Majesty's Theatre 2012-Present
-                <br>
-                Breakin'Convention Sadlers Wells 2004-2014
-                <br>
-                UK Music and Dance Festivals 2000-Present</p>
+              <h5 class="credits__sub-heading"><?php the_field('credits_section_2_heading');?></h5>
+              <?php the_field('credits_section_2_text');?>
             </div>
           
             <div class="credits__section">
-              <h5 class="credits__sub-heading">Choreography</h5>
-              
-              <p>Impact Dance "One Day" Bristol Harbour Festival 2015
-                <br> 
-                Summit for Breakin Convention 2013
-                <br> 
-                Olympic Fans Park Opening in Association with Coke Cola 2012
-                <br> 
-                Smash for Breakin’convention 2009
-                <br> 
-                Flo-Rida “Lo” MOBO Awards Wembley Arena 2008
-                <br> 
-                Pet Shop Boys World Tour Fundamental  2006</p>
+              <h5 class="credits__sub-heading"><?php the_field('credits_section_3_heading');?></h5>
+              <?php the_field('credits_section_3_text');?>
             </div>
 
             <div class="credits__section">
-              <h5 class="credits__sub-heading">Ambassadorial</h5>
-
-              <p>Hip Hop Project British Council- Caracas Venezuela 2016
-              <br>  
-              Say it Through Breakdance 2014-Present Tunisia British Council
-              <br>  
-              Live Vibe Harare Zimbabwe 2011-2012
-              <br>  
-              Live Vibe Hong Kong 2010 & 2012
-              <br>  
-              Cultural Leadership International British Council 2009
-              </p>
+              <h5 class="credits__sub-heading"><?php the_field('credits_section_4_heading');?></h5>
+              <?php the_field('credits_section_4_text');?>
             </div>
             
             <div class="credits__section">
-              <h5 class="credits__sub-heading">Nominations</h5>
-              <p>Genesis Prize 2014: Nominated
-              <br>
-              Paul Hamlyn Breakthrough Support 2013: Nominated</p>
+              <h5 class="credits__sub-heading"><?php the_field('credits_section_5_heading');?></h5>
+              <?php the_field('credits_section_5_text');?>
             </div>
 
             <div class="credits__section">
-              <h5 class="credits__sub-heading">Teaching/Workshops</h5>
-
-              <p>
-              Royal Ballet School Associate Programme 2015
-              <br>
-              Royal Ballet School Dance Access and Partnership/Summer School 2013
-              <br>
-              British Council Zimbabwe. Developmental workshops/Live Vibe Project Feb/Apr 2012
-              <br>
-              Big Dance Bus workshops Big Dance 2012
-              <br>
-              Laban international summer school 2012
-              <br>
-              Coke Cola for Olympics Hyde park 2012
-              <br>
-              ESCAPE Holiday Dalaman Turkey Master classes 2012/2013/2014
-              <br>
-              British Council Connecting Classrooms School’s pledge Beirut Lebanon 2010
-              <br>
-              Hong Kong Live Vibe Dance workshops 2013/2010
-              <br>
-              Resident Choreographer for Street Dance International sponsored by Malta Guinness 2011 (Kenya) 2010 (Ghana) 2009 (Ghana)
-              <br>
-              Disability in sports Islington Disabled children access to dance 2012/2013/2014
-              </p>
+              <h5 class="credits__sub-heading"><?php the_field('credits_section_6_heading');?></h5>
+              <?php the_field('credits_section_6_text');?>
             </div>
         </div>
       </div>
